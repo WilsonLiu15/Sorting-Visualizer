a78695bc3cfb26d813d7f44b82848f654ca5f995
IronDefender15 <68487472+IronDefender15@users.noreply.github.com>
2021-05-14 14:50:36 -0400

    Incorporating Styled Select Box Component

---
+++ src/Components/GlowSelectBox/GlowSelectBox.js
@@ -0,0 +1,21 @@
+import React from "react"
+
+import "./GlowSelectBox.scss"
+
+const GlowSelectBox = (props) => {
+
+    return (
+        <div class="custom-select">
+            <select className="selectAlgorithm" onChange= {props.handleAlgorithm} value={props.algorithm}>
+                <option value='bubbleSort'>Bubble Sort</option>
+                <option value='heapSort'>Heap Sort</option>
+                <option value='insertionSort'>Insertion Sort</option>
+                <option value='selectionSort'>Selection Sort</option>
+                <option value='mergeSort'>Merge Sort</option>
+                <option value='quickSort'>Quick Sort</option>
+            </select>
+        </div>
+    )
+}
+
+export default GlowSelectBox
