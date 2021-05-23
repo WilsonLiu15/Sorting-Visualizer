9ff9aaa56b7502e9b3ee28932fc3b4868d509daa
IronDefender15 <68487472+IronDefender15@users.noreply.github.com>
2021-05-23 17:33:22 -0400

    Implement Select Box

--- src/Components/GlowSelectBox/GlowSelectBox.js
+++ src/Components/GlowSelectBox/GlowSelectBox.js
@@ -5,14 +5,18 @@ import "./GlowSelectBox.scss"
 const GlowSelectBox = (props) => {
 
     return (
-        <div class="custom-select">
-            <select className="selectAlgorithm" onChange= {props.handleAlgorithm} value={props.algorithm}>
-                <option value='bubbleSort'>Bubble Sort</option>
-                <option value='heapSort'>Heap Sort</option>
-                <option value='insertionSort'>Insertion Sort</option>
-                <option value='selectionSort'>Selection Sort</option>
-                <option value='mergeSort'>Merge Sort</option>
-                <option value='quickSort'>Quick Sort</option>
+        <div className="custom-select">
+            <select 
+                className="selectAlgorithm" 
+                onChange= {props.handleAlgorithm} 
+                value={props.algorithm}
+                disabled={props.sorting}>
+                    <option value='bubbleSort'>Bubble Sort</option>
+                    <option value='heapSort'>Heap Sort</option>
+                    <option value='insertionSort'>Insertion Sort</option>
+                    <option value='mergeSort'>Merge Sort</option>
+                    <option value='quickSort'>Quick Sort</option>
+                    <option value='selectionSort'>Selection Sort</option>
             </select>
         </div>
     )
